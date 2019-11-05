3339283f70363b72b1be4aa77328e24fb381e0f1
Paul Thomas <traitorpt@gmail.com>
2019-11-05 13:57:26 -0800

    Updates

---
+++ precache-manifest.fb212720849590766c29654bfdc71cb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736e556831b83d34cf2760d03cb2ee38",
+    "url": "/potter-clicky-game/index.html"
+  },
+  {
+    "revision": "6939cbcaf4184e0f1235",
+    "url": "/potter-clicky-game/static/css/main.1300254e.chunk.css"
+  },
+  {
+    "revision": "22a7a8b4262a2473f0a2",
+    "url": "/potter-clicky-game/static/js/2.6fa803cc.chunk.js"
+  },
+  {
+    "revision": "6939cbcaf4184e0f1235",
+    "url": "/potter-clicky-game/static/js/main.d5e68ca8.chunk.js"
+  },
+  {
+    "revision": "93e5cf6dc5a2920b7b2b",
+    "url": "/potter-clicky-game/static/js/runtime-main.916a63fb.js"
+  }
+]);
